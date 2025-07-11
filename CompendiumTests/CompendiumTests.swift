ca47b038aa636ec0df7386bd4a8b7446f4fcfffc
Penny Marshall <pmarshall@digitalpromise.org>
2025-07-11 10:53:23 -0400

    Initial Commit

---
+++ CompendiumTests/CompendiumTests.swift
@@ -0,0 +1,17 @@
+//
+//  CompendiumTests.swift
+//  CompendiumTests
+//
+//  Created by Penny Marshall on 7/11/25.
+//
+
+import Testing
+@testable import Compendium
+
+struct CompendiumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
